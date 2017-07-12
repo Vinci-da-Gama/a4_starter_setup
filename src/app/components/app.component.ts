d193a3946e72e33f8f64ac2765be191391ca46f4
Occ Coo <kylingourmand@gmail.com>
2017-07-13 07:48:48 +1000

    style url

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
  @Component({
  	selector: 'my-app',
  	templateUrl: './app.component.html',
- 	styleUrls: [require('./app.component.scss')]
+ 	styleUrls: ['./app.component.scss']
  })
  export class AppGeneralComponent {
  	constructor() {}
